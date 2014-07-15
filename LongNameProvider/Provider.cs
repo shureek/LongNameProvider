57b1ef061e506279efd9f010ccb5470fe2e797f2
shureek <shureek@gmail.com>
2014-07-15 07:49:44 +0400

    Version 0.1.0.0

---
+++ LongNameProvider/Provider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+
+namespace LongNameProvider
+{
+    [CmdletProvider("LongNameProvider", ProviderCapabilities.Include | ProviderCapabilities.ShouldProcess)]
+    public class LongNameProvider : NavigationCmdletProvider
+    {
+        protected override bool IsValidPath(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void CopyItem(string path, string copyPath, bool recurse)
+        {
+            base.CopyItem(path, copyPath, recurse);
+        }
+
+        protected override object CopyItemDynamicParameters(string path, string destination, bool recurse)
+        {
+            return base.CopyItemDynamicParameters(path, destination, recurse);
+        }
+    }
+}
